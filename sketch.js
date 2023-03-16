1b03e6d798ba92b4ab41656b4121270031c6d62d
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-15 21:53:09 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,10 +3,12 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world, backgroundImg,boat;
+var engine, world, backgroundImg;
 var canvas, angle, tower, ground, cannon;
 var balls = [];
 var boats = [];
+
+var score = 0;
 
 function preload() {
   backgroundImg = loadImage("./assets/background.gif");
@@ -20,6 +22,7 @@ function setup() {
   angleMode(DEGREES)
   angle = 15
 
+
   ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
   World.add(world, ground);
 
@@ -27,7 +30,6 @@ function setup() {
   World.add(world, tower);
 
   cannon = new Cannon(180, 110, 130, 100, angle);
- 
 }
 
 function draw() {
@@ -36,30 +38,42 @@ function draw() {
 
   Engine.update(engine);
 
- 
-  rect(ground.position.x, ground.position.y, width * 2, 1);
-  
-
   push();
-  imageMode(CENTER);
-  image(towerImage,tower.position.x, tower.position.y, 160, 310);
+  translate(ground.position.x, ground.position.y);
+  fill("brown");
+  rectMode(CENTER);
+  rect(0, 0, width * 2, 1);
   pop();
 
-
+  push();
+  translate(tower.position.x, tower.position.y);
+  rotate(tower.angle);
+  imageMode(CENTER);
+  image(towerImage, 0, 0, 160, 310);
+  pop();
 
   showBoats();
 
   for (var i = 0; i < balls.length; i++) {
-    showCannonBalls(balls[i], i);
+    //???
   }
 
+  /* collisionWithBoat é chamada passando o índice da bola como argumento.
+   Essa função é responsável por verificar se a bola colidiu com algum barco, e remover a bola e o barco do mundo em caso de colisão.*/
   cannon.display();
+
+
+}
+
+function collisionWithBoat(index) {
+  for (var i = 0; i < boats.length; i++) {
+    //???
+  }
 }
 
 function keyPressed() {
   if (keyCode === DOWN_ARROW) {
-    //Crie um objeto da classe CannonBall e seus atributos serão 'cannon.x' e 'cannon.y'
-    //???
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
     cannonBall.trajectory = [];
     Matter.Body.setAngle(cannonBall.body, cannon.angle);
     balls.push(cannonBall);
@@ -67,9 +81,7 @@ function keyPressed() {
 }
 
 function showCannonBalls(ball, index) {
-  if (ball) {
-    ball.display();
-  }
+  //???
 }
 
 function showBoats() {
@@ -81,24 +93,24 @@ function showBoats() {
       var positions = [-40, -60, -70, -20];
       var position = random(positions);
       var boat = new Boat(width, height - 100, 170, 170, position);
-//Coloque o código 'push', pois ele adiciona o barco dentro da lista boats
-      boats.???(boat);
+
+      boats.push(boat);
     }
 
     for (var i = 0; i < boats.length; i++) {
       if (boats[i]) {
-        //Utilize o código que define a velocidade
-        ???(boats[i].body, {
+        Matter.Body.setVelocity(boats[i].body, {
           x: -0.9,
           y: 0
         });
 
         boats[i].display();
-      } 
+      } else {
+        boats[i];
+      }
     }
   } else {
-    //Crie um objeto para o boat com os seguintes atributos: 'width, height - 60, 170, 170, -60'
-    //???
+    var boat = new Boat(width, height - 60, 170, 170, -60);
     boats.push(boat);
   }
 }
